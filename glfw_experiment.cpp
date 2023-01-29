079457ba5cdedb3626c0b0a4b4cf8057488043a0
Jason Han <jjhan@vassar.edu>
2023-01-29 01:09:13 -0500

    Add multiple textures

--- glfw_experiment.cpp
+++ glfw_experiment.cpp
@@ -87,11 +87,9 @@ int main() {
                           (void*)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
 
-    unsigned int texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    // set the texture wrapping/filtering options (on the currently bound
-    // texture object)
+    unsigned int texture1, texture2;
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
@@ -99,6 +97,7 @@ int main() {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
     int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);
     unsigned char* data =
         stbi_load("resources/container.jpg", &width, &height, &nrChannels, 0);
     if (data) {
@@ -110,13 +109,40 @@ int main() {
     }
     stbi_image_free(data);
 
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    data = stbi_load("resources/awesomeface.png", &width, &height, &nrChannels,
+                     0);
+    if (data) {
+        // note that the awesomeface.png has transparency and thus an alpha
+        // channel, so make sure to tell OpenGL the data type is of GL_RGBA
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA,
+                     GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    // don't forget to activate/use the shader before setting uniforms!
+    shader.use();
+    shader.set_1int("texture2", 1);
+
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBindTexture(GL_TEXTURE_2D, texture);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
 
         shader.use();
         glBindVertexArray(VAO);
--- resources/shader.fs
+++ resources/shader.fs
@@ -5,8 +5,10 @@ in vec2 tex_coord;
 
 out vec4 frag_color;
 
-uniform sampler2D our_texture;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main() {
-    frag_color = texture(our_texture, tex_coord);
+    frag_color =
+        mix(texture(texture1, tex_coord), texture(texture2, tex_coord), 0.2);
 }
